44d66a7d2df4679755bcf3e538443a4b9c2763ad
Lanalumi <lanalumi97@gmail.com>
2025-03-24 23:16:04 +0000

    add links repositorios dinâmico

--- scripts/myprojects.js
+++ scripts/myprojects.js
@@ -1,17 +1,21 @@
 const url="https://api.github.com/users/lanalumi/repos"
 const request=fetch(url).then(function(value){
     return value.json()
-}).then(function(data){
-console.log(data)
-
-const listItem = document.createElement("li")
-listItem.classList.add("project")
-listItem.innerHTML = `
-<a class="project-link" href="" target="_blank">
-    Formulário+login+Menu
-    <img src="./assets/external-link.svg">
-</a>
-`
-const projectsContainer = document.querySelector("[data-projects]")
-projectsContainer.appendChild(listItem)
+}).then(function(data){    
+    data.forEach(function(repo){
+        
+        const listItem = document.createElement("li")
+        listItem.classList.add("project")
+        listItem.innerHTML = `
+        <a class="project-link" href="${repo.svn_url}" target="_blank">
+            ${repo.name}
+            <img src="./assets/external-link.svg">
+        </a>
+        `
+        const projectsContainer = document.querySelector("[data-projects]")
+        projectsContainer.appendChild(listItem)
+        
+    }) 
 })
+
+
